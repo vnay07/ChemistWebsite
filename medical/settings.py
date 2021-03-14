17e9a7577dae05244cff9ec164676cd5c8be914c
Vinay <vnaykanswal55@gmail.com>
2021-03-14 23:05:55 +0530

    Debug set to true

--- medical/settings.py
+++ medical/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['newdoonchemist.herokuapp.com','127.0.0.1']
 
