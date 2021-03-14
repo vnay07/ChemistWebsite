21b42dc7ac038c5bcb816040f4d51c1a06c04750
Vinay <vnaykanswal55@gmail.com>
2021-03-14 22:48:56 +0530

    Added Requirement And Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn medical.wsgi --log-file -
--- medical/settings.py
+++ medical/settings.py
@@ -33,9 +33,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['newdoonchemist.herokuapp.com','127.0.0.1']
 
 
 # Application definition
